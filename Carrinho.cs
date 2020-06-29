91be9a227bcbee22c74b8b8508fa7dde347eb99b
LuannaCorreia <lusinha995@gmail.com>
2020-06-29 14:54:02 -0300

    Aula 24 finalizada

---
+++ Carrinho.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace Aula24
+{
+    public class Carrinho
+    {
+        public float ValorTotal { get; set; }
+
+        List<Produto> carrinho = new List<Produto>();
+
+        public void Adicionar(Produto _produto){
+            carrinho.Add(_produto);
+        }
+
+        public void Remover(Produto _produto){
+            carrinho.Remove(_produto);
+        }
+
+        public void Ler(){
+            foreach(Produto item in carrinho){
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine($"{item.Nome} - R$ {item.Preco}");
+                Console.ResetColor();
+            }
+        }
+
+        public void Alterar(int _codigo, Produto _produtoAlterado){
+            carrinho.Find(z => z.Codigo == _codigo).Nome = _produtoAlterado.Nome;
+            carrinho.Find(z => z.Codigo == _codigo).Preco = _produtoAlterado.Preco;
+        }
+        
+        public void MostrarTotal(){
+            foreach(Produto item in carrinho){
+                ValorTotal += item.Preco;
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            System.Console.WriteLine($"Valor total dos itens: R$ {ValorTotal}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             Carrinho cart = new Carrinho();
+            
+            Produto p1 = new Produto(1,  "Iphone 11 Pro Max", 7500.99f);
+            Produto p2 = new Produto(2, "Samsung A51", 3900.99f);
+            Produto p3 = new Produto(3, "Lg ", 3900.99f);
+            Produto p4 = new Produto(4, "Motorola", 3900.99f);
+
+            cart.Adicionar(p1);
+            cart.Adicionar(p2);
+            cart.Adicionar(p3);
+            cart.Adicionar(p4);
+
+            cart.Remover(p3);
+
+            Produto p5 = new Produto(1,  "Iphone 11 Pro Max", 7500.99f);
+            cart.Alterar(1, p5);
+
+            cart.Ler();
+            cart.MostrarTotal();
+        }
+    }
+}
